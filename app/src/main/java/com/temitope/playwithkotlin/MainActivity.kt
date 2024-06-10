101d012b2dedb28e81df568779682cf40e67a738
Temitopeajayi <topesamuelajayi@gmail.com>
2024-06-10 08:07:17 +0100

    Basic roll die app

---
+++ app/src/main/java/com/temitope/playwithkotlin/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.temitope.playwithkotlin
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+       val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{
+            rollDice()
+        }
+
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun rollDice() {
+
+        val randomText = Random.nextInt(6) + 1
+         var drawableResource = when (randomText) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+             else -> R.drawable.dice_6
+
+         }
+        diceImage.setImageResource(drawableResource)
+
+        }
+
+
+}
